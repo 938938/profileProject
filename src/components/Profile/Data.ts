1d34a960187f505378d04c1422940988a7c9dd70
938938 <938kjh@gmail.com>
2023-07-10 23:27:44 +0900

    [feat] 데이터 추가

---
+++ src/components/Profile/Data.ts
@@ -0,0 +1,41 @@
+export const profile = {
+  name: '',
+  img: '',
+  info: '',
+  email: '',
+  github: '',
+  blog: '',
+  skill: '',
+  etc: [
+    {
+      name: '',
+      date: '',
+      sub: '',
+    },
+  ],
+};
+
+export const education = [
+  {
+    name: '',
+    term: '',
+    info: '',
+    learn: [''],
+  },
+];
+export const project = [
+  {
+    type: 'team',
+    title: '',
+    info: '',
+    skill: '',
+    link: {
+      github: '',
+      site: '',
+      blog: '',
+    },
+    term: '',
+    position: '',
+    list: '',
+  },
+];
